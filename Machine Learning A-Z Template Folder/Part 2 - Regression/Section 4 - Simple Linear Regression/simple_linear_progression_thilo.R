e365218d2632bbe193b482be849897eba5bbed30
ItsNothingReally <vash@Thilos-Mac-mini.local>
2017-05-25 16:20:10 +0900

    Didn't work

---
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_progression_thilo.R
@@ -0,0 +1,40 @@
+# Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+# dataset = dataset[, 2:3]
+
+# Taking care of missing data
+# dataset$Age = ifelse(is.na(dataset$Age),
+#                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+#                      dataset$Age)
+# dataset$Salary = ifelse(is.na(dataset$Salary),
+#                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+#                      dataset$Salary)
+
+# Encoding categorical data
+# dataset$Country = factor(dataset$Country,
+#                          levels = c('France', 'Spain', 'Germany'),
+#                          labels = c(1, 2, 3))
+# dataset$Purchased = factor(dataset$Purchased,
+#                            levels = c('No', 'Yes'),
+#                            labels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+# Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
